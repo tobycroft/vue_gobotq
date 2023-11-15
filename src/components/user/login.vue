6022fa773d2ed045d554ff4dc3a1cc3b7fcb1bb5
tuuz <silverhawk@foxmail.com>
2023-11-16 01:15:59 +0800

    Initial commit

---
+++ src/components/user/login.vue
@@ -0,0 +1,108 @@
+<template>
+
+  <v-container class="align-self-center">
+    <v-card v-if="show1"
+            class="mx-auto"
+            max-width="200"
+            height="200"
+            image="@/assets/gobotq.png"
+            title="GobotQ"
+            theme="dark"
+    ></v-card>
+
+    <v-sheet width="100%" class="mx-auto mt-6" v-if="show1">
+      <v-form @submit.prevent>
+        <v-text-field
+          v-model="qq"
+          :rules="rules"
+          label="这里输入你的QQ号码"
+        ></v-text-field>
+        <v-btn type="submit" block class="mt-0" color="blue" @click="gotonext()">下一步</v-btn>
+        <v-btn type="submit" block class="mt-4" color="grey" @click="clearout()">清空数据</v-btn>
+      </v-form>
+    </v-sheet>
+
+    <v-card v-if="show2"
+            class="py-8 px-6 text-center mx-auto ma-4"
+            elevation="12"
+            max-width="100%"
+            width="100%"
+    >
+      <h3 class="text-h6 mb-4">填写你QQ的登录码</h3>
+
+      <div class="text-body-2">
+        <v-col>不是你的QQ密码！！！</v-col>
+        <v-col>登录码请通过已在线的Acfur机器人获取</v-col>
+        <v-col>向机器人发送“acfur登录”，并等待机器人回复验证码给你</v-col>
+      </div>
+
+      <v-sheet color="surface">
+        <v-otp-input
+          v-model="otp"
+          type="tel"
+          variant="solo"
+        ></v-otp-input>
+      </v-sheet>
+
+      <v-btn @click="login()"
+        class="my-4"
+        color="blue"
+        height="40"
+        text="验证并登录"
+        variant="flat"
+        width="70%"
+      ></v-btn>
+
+      <v-btn @click="clearout()"
+        class="my-4"
+        color="grey"
+        height="40"
+        text="返回"
+        variant="flat"
+        width="70%"
+      ></v-btn>
+
+    </v-card>
+
+  </v-container>
+</template>
+
+
+<script>
+export default {
+  created() {
+    this.qq = localStorage.getItem("qq")
+  },
+  data: () => ({
+    show1: true,
+    show2: false,
+    qq: '',
+    otp: '',
+    rules: [
+      value => {
+        if (value) return true
+        return '这里填写的应该是你的QQ号码，而不是你机器人的QQ号码'
+      },
+    ],
+  }),
+  methods: {
+    gotonext() {
+      if (this.qq.length > 5) {
+        this.show1 = false;
+        this.show2 = true
+        localStorage.setItem("qq", this.qq)
+      }
+    },
+    clearout() {
+      this.show2 = false
+      this.show1 = true
+      this.qq = ''
+      localStorage.removeItem('qq')
+    },
+    login() {
+      this.qq = ''
+      localStorage.removeItem('qq')
+    },
+  },
+}
+</script>
---
+++ src/views/user/Login.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Logincomp from "@/components/user/login.vue";
+</script>
+
+<template>
+  <v-toolbar title="GoBotQ控制面板"></v-toolbar>
+    <v-layout class="rounded rounded-md">
+      <v-main class="d-flex align-center justify-center">
+        <logincomp></logincomp>
+      </v-main>
+    </v-layout>
+
+</template>
+<style scoped>
+
+</style>
