361e0a894376c92c69d0d16a8eb3f85e314c964a
tuuz <silverhawk@foxmail.com>
2023-11-17 00:16:52 +0800

    TuuzPC

--- src/router/index.js
+++ src/router/index.js
@@ -4,42 +4,43 @@ import {createRouter, createWebHistory} from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('@/views/user/Login.vue'),
+    name:'login',
+    component: () => () => import('@/views/user/Login.vue'),
   },
   {
     path: '/user/login',
-    component: import('@/views/user/Login.vue'),
+    name:'login2',
+    component: () => import('@/views/user/Login.vue'),
   },
   {
     path: '/center',
-    component: import('@/views/Center.vue'),
+    name:'center',
+    component: () => import('@/views/Center.vue'),
   },
   {
     path: '/center/bot',
-    component: import('@/views/v1/Bot.vue'),
+    name:'bot',
+    component: () => import('@/views/v1/Bot.vue'),
   },
   {
     path: '/center/group',
-    component: import('@/views/v1/Group.vue'),
+    name:'group',
+    component: () => import('@/views/v1/Group.vue'),
   },
   {
     path: '/center/index',
-    component: import('@/views/v1/Index.vue'),
+    name:'index',
+    component: () => import('@/views/v1/Index.vue'),
   },
   {
     path: '/center/user',
-    component: import('@/views/v1/User.vue'),
+    name:'user',
+    component: () => import('@/views/v1/User.vue'),
   },
   {
     path: '/v1/bot/detail',
-    component: import('@/views/v1/bot/detail.vue'),
-  },
-  {
-    path: '/v1/bot/detail',
-    component: import('@/views/v1/User.vue'),
-  }, {
-    path: '/v1/bot/detail',
-    component: import('@/views/v1/User.vue'),
+    name:'detail',
+    component: () => import('@/views/v1/bot/detail.vue'),
   },
 ]
 
