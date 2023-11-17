fff1c625dd4cadf0d0a6427be9d99fbfa1c1e6f1
tuuz <silverhawk@foxmail.com>
2023-11-17 19:42:21 +0800

    TuuzPC

--- src/views/v1/group/subpage/edit.vue
+++ src/views/v1/group/subpage/edit.vue
@@ -37,53 +37,19 @@ import Alert from "@/plugins/Alert";
 export default {
   data() {
     return {
+      valid: true,
       formData: {
-        ban_repeat: 1,
-        auto_card_insert: 0,
-        ban_group: 0,
-        ban_time: 60,
-        ban_share: 0,
-        exit_alert: 0,
-        id: 14,
-        sign_send_private: 0,
-        auto_verify: 1,
-        auto_kick_out: 1,
-        auto_verify_word: "",
-        join_alert: 0,
-        kick_to_black: 0,
-        auto_card: 0,
-        group_id: 94537310,
-        sign: 1,
-        auto_retract: 1,
-        welcome_word: "欢迎加入火线兔的宇宙飞船",
-        repeat_count: 2,
-        auto_card_value: "请修改群名片",
-        word_limit: 1000,
-        ban_retract: 1,
-        auto_welcome: 1,
-        sign_send_retract: 1,
-        ad_retract: 1,
-        exit_to_black: 0,
-        auto_hold: 1,
-        repeat_time: 10,
-        adblock: 1,
-        all_send_private: 0,
-        welcome_at: 1,
-        auto_join: 1,
-        ban_wx: 0,
-        ban_limit: 3,
-        ban_url: 0,
+        self_id: 0,
+        group_id: 0,
       },
       fields: [],
       field: {},
     };
   },
   methods: {
-    updateFormData(key, value) {
-    },
     async submitForm() {
       // This is where you call your own submission method and pass this.formData to it
-      var ret = await new Net("/v1/bot/info/edit").PostFormData(this.formData)
+      var ret = await new Net("/v1/group/function/edit").PostFormData(this.formData)
       if (ret.isSuccess) {
         Alert.SetAlert(ret.echo)
         this.$router.go(-1)
@@ -117,30 +83,26 @@ export default {
       if (ret.isSuccess) {
         let list = ret.data
         for (const listKey in list) {
-          console.log(listKey)
-          console.log(this.field[listKey])
           switch (this.field[listKey]) {
-            case "string":
-              break
-
             case "bool":
-              list[listKey] = Boolean(list[listKey])
-              console.log(listKey)
+              this.formData[listKey] = Boolean(list[listKey])
               break
 
-            case "int":
+            default:
+              this.formData[listKey] = list[listKey]
               break
           }
         }
-        this.formData = list
       } else {
         Alert.SetAlert(ret.echo)
       }
     },
   },
   created() {
-    // Call fillForm method to perform data filling when the component is mounted
-    this.getDetail();
+    this.formData.self_id = this.$route.query.self_id,
+      this.formData.group_id = this.$route.query.group_id,
+      // Call fillForm method to perform data filling when the component is mounted
+      this.getDetail();
   },
 };
 </script>
