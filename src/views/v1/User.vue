5946b5f687c73b5b728c439091cb9ddf20126430
tuuz <silverhawk@foxmail.com>
2023-11-19 17:10:35 +0800

    TuuzPC

--- src/views/v1/User.vue
+++ src/views/v1/User.vue
@@ -1,27 +1,46 @@
-<script>
-
-import {defineComponent} from "vue";
-import Topheader from "@/components/center/header/topheader.vue";
-
-export default defineComponent({
-  components: {Topheader}
-})
-</script>
-
 <template>
-  <v-app>
-    <!-- 顶部导航栏 -->
-    <Topheader></Topheader>
-
-    <!-- 主页面内容区域 -->
-    <v-main>
-      <!-- 根据路由显示不同的页面 -->
-      <router-view>
-      </router-view>
-    </v-main>
-  </v-app>
+  <Topheader></Topheader>
+  <!-- 显示 QQ 号码 -->
+  <v-row justify="center">
+    <v-col cols="12">
+      <v-card>
+        <v-card-title>个人信息</v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                label="QQ号码"
+                v-model="qqNumber"
+                readonly
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <!-- 退出按钮 -->
+          <v-btn @click="logout" color="primary">退出</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
-<style scoped>
+<script>
+import Topheader from "@/components/center/header/topheader.vue";
 
-</style>
+export default {
+  components: {Topheader},
+  data() {
+    return {
+      qqNumber: 'YourQQNumber', // 替换为实际的 QQ 号码
+    };
+  },
+  methods: {
+    // 退出按钮点击事件
+    logout() {
+      // 处理退出逻辑，可以调用接口等操作
+      // 例如：this.$router.push('/login') 跳转到登录页
+    },
+  },
+};
+</script>
